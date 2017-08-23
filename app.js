86a65cf6901ae8348efc78107c65ff04aebefecc
BroKun <wangyukun1234@hotmail.com>
2017-08-23 22:55:05 +0800

    update

--- app.js
+++ app.js
@@ -1,44 +1,34 @@
+const { checkSession, login, getUserInfo } = require('./lib/wx.js');
+const co = require('./lib/co.js');
 //app.js
 App({
-  onLaunch: function() {
+  onLaunch: co.wrap(function* () {
     //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-    wx.login({
-      success: function (res) {
-        if (res.code) {
-          // //发起网络请求
-          // wx.request({
-          //   url: 'https://test.com/onLogin',
-          //   data: {
-          //     code: res.code
-          //   }
-          // })
-          console.log(res);
-        } else {
-          console.log('获取用户登录态失败！' + res.errMsg)
-        }
+    var logs = wx.getStorageSync('logs') || [];
+    logs.unshift(Date.now());
+    wx.setStorageSync('logs', logs);
+    const token = wx.getStorageSync('logs');
+    if (!token) {
+      const loginRes = yield login();
+      if (loginRes.code) {
+        // TODO: request code 转化为用户信息，获取并保存token
+      } else {
+        // TODO: 无法正确的进行首次登录
       }
-    });
-  },
+    }
+  }),
 
-  getUserInfo: function(cb) {
+  getUserInfo: co.wrap(function* () {
     var that = this
     if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo)
+      return this.globalData.userInfo;
     } else {
       //调用登录接口
-      wx.getUserInfo({
-        withCredentials: false,
-        success: function(res) {
-          that.globalData.userInfo = res.userInfo
-          console.log(res);
-          typeof cb == "function" && cb(that.globalData.userInfo)
-        }
-      })
+      const res = yield getUserInfo({ withCredentials: true });
+      that.globalData.userInfo = res.userInfo;
+      return this.globalData.userInfo;
     }
-  },
+  }),
 
   globalData: {
     userInfo: null
