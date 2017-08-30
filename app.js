4fa7fc75e5d38379ebbe1ee106b105f9cb876fb2
BroKun <wangyukun1234@hotmail.com>
2017-08-30 18:31:25 +0800

    账户初始化与维护变更

--- app.js
+++ app.js
@@ -10,35 +10,57 @@ function getOpenId(code) {
 //app.js
 App({
   onLaunch: co.wrap(function* () {
-    console.log('onLaunch');
+    console.log('首次启动');
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || [];
     logs.unshift(Date.now());
     wx.setStorageSync('logs', logs);
-    const accountInfo = wx.getStorageSync(accountKey);
     try {
-      if (!accountInfo) {
-        const loginRes = yield login();
-        if (loginRes.code) {
-          const infos = yield getOpenId(loginRes.code);
-          wx.setStorageSync(accountKey, infos);
-          Object.assign(this.globalData, infos);
-        } else {
-          throw new Error('login failed');
-        }
+      const loginRes = yield login();
+      if (loginRes.code) {
+        const infos = yield getOpenId(loginRes.code);
+        wx.setStorageSync(accountKey, infos);
+        Object.assign(this.globalData, infos);
       } else {
-        Object.assign(this.globalData, accountInfo);
+        throw new Error('login failed');
       }
-    } catch (ex) {
+    }
+    catch(ex){
       // TODO: 给出无法登陆的错误页
     }
   }),
-
+  onShow: () => {
+    console.log('进入程序');
+  },
+  onHide: () => {
+    console.log('隐藏程序');
+  },
+  onError: () => {
+    console.log('程序异常');
+  },
   getUserInfo: co.wrap(function* () {
     var that = this
     if (this.globalData.userInfo) {
       return this.globalData.userInfo;
     } else {
+      const accountInfo = wx.getStorageSync(accountKey);
+      try {
+        if (!accountInfo) {
+          yield checkSession();
+          const loginRes = yield login();
+          if (loginRes.code) {
+            const infos = yield getOpenId(loginRes.code);
+            wx.setStorageSync(accountKey, infos);
+            Object.assign(this.globalData, infos);
+          } else {
+            throw new Error('login failed');
+          }
+        } else {
+          Object.assign(this.globalData, accountInfo);
+        }
+      } catch (ex) {
+        // TODO: 给出无法登陆的错误页
+      }
       //调用登录接口
       const res = yield getUserInfo({ withCredentials: true });
       that.globalData.userInfo = res.userInfo;
@@ -47,6 +69,9 @@ App({
   }),
 
   globalData: {
-    userInfo: null
+    userInfo: null,
+    openid: null,
+    unionid: null,
+    token: null,
   }
 })
